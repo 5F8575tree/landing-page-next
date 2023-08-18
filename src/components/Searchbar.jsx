094f0462e2e46e99447eca32d9c0369d646c2608
Mark Rawlins <mark.jeffrey.rawlins@gmail.com>
2023-08-18 11:25:08 +0900

    finish search bar

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -3,20 +3,42 @@ import Image from "next/image";
 const Searchbar = () => {
   return (
     <section className="searchbar flex justify-center mt-6">
-      <form
-        action="/cars"
-        className="blue-text bg-white w-10/12 rounded-lg drop-shadow"
-      >
-        <fieldset>
-          <div className="search-container flex justify-between content-center p-3">
-            <input
-              type="search"
-              placeholder="Search by make, model, or keyword"
-              className="w-full"
-            />
-            <Image src="/search.svg" alt="search icon" width="20" height="20" />
-          </div>
-        </fieldset>
+      <form action="" className="w-10/12">
+        <div className="relative flex items-center">
+          <input
+            type="text"
+            name="search"
+            placeholder="Search by make, model, or keyword"
+            autocomplete="off"
+            aria-label="search talk"
+            className="px-3
+              py-2
+              placeholder-slate-600
+              hover:placeholder-slate-300
+              focus:placeholder-slate-300
+              text-black
+              rounded-2xl
+              border-none
+              ring-2
+              ring-slate-300
+              w-full
+              drop-shadow
+              focus:ring-slate-500
+              focus:ring-2
+              bg-slate-50
+              hover:bg-white
+              pr-3
+              pl-10
+              "
+          />
+          <Image
+            src="/search.svg"
+            alt="search icon"
+            width="24"
+            height="24"
+            className="absolute right-10 pointer-events-none"
+          />
+        </div>
       </form>
     </section>
   );

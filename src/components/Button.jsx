aca1fc7be8a8f8393feacf0301e5123c59a51206
Mark Rawlins <mark.jeffrey.rawlins@gmail.com>
2023-08-18 21:31:37 +0900

    add smaller image for small screens and adjust catch-copy font color and size

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 const Button = () => {
   return (
     <div className="flex justify-center">
-      <button className="text-center text-white text-sm font-normal bg-red-500 hover:bg-red-400 py-3 px-20 rounded mt-8">
+      <button className="text-center text-white text-sm font-normal bg-red-500 hover:bg-red-400 py-3 px-20 rounded mt-8 sm:drop-shadow-lg">
         SEARCH CARS
       </button>
     </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,24 +3,31 @@ import Navbar from "./Navbar";
 import Searchbar from "./Searchbar";
 
 const Hero = () => {
-  const backgroundImg = "url(/car.jpeg)";
-
-  const containerStyle = {
-    backgroundImage: backgroundImg,
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-  };
-
   return (
-    <div className="w-screen h-screen px-48" style={containerStyle}>
+    <div
+      className="w-screen h-screen xl:px-48 lg:px-16 md:px-8 sm:px-4"
+      id="hero-image"
+    >
       <Navbar />
       <Searchbar />
-      <div className="h-4/6 flex flex-col items-start justify-center">
-        <h2 className="text-7xl text-white drop-shadow-md font-bold">
+      <div className="h-4/6 flex flex-col md:items-start md:justify-center sm:justify-start sm:mt-10">
+        <h2
+          className="
+          font-bold
+          md:text-7xl
+          md:text-white
+          md:drop-shadow-md
+          md:text-left
+          sm:text-5xl
+          sm:text-blue-900
+          sm:drop-shadow-lg
+          sm:text-center
+          "
+        >
           Love Your Car <br />
           Guarantee
         </h2>
-        <h4 className="text-xl text-white text-center pt-2">
+        <h4 className="text-center pt-2 md:text-xl md:text-white sm:text-base sm:text-blue-900 sm:drop-shadow-lg">
           Take 30 days to love it or return it (up to 1500 mi)
         </h4>
         <Button />

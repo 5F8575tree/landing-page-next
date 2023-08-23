fa587ba8851c12914c7ecfd00d9b2de40d43d84a
Mark Rawlins <mark.jeffrey.rawlins@gmail.com>
2023-08-23 10:21:57 +0900

    add in bg-image

---
+++ src/components/ReasonToBelieve/ReasonToBelieve.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+const ReasonToBelieve = () => {
+  return (
+    <div className="mt-16 h-screen" id="reasons-to-believe">
+      Reason to Believe
+    </div>
+  );
+};
+
+export default ReasonToBelieve;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import BudgetSearch from "@/components/BudgetSearch/BudgetSearch";
 import Hero from "@/components/Hero/Hero";
+import ReasonToBelieve from "@/components/ReasonToBelieve/ReasonToBelieve";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -9,6 +10,7 @@ export default function Home() {
     <main>
       <Hero />
       <BudgetSearch />
+      <ReasonToBelieve />
     </main>
   );
 }
